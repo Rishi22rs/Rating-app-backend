3ccb8b9c6116d18330da4349dc8ea694f8a37993
Rishi Srivastava <rishisrivastava22rs@gmail.com>
2020-10-03 05:13:56 +0530

    please work

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const { addImages, selectRandom, vote } = require("./controller");
+//const { addImages, selectRandom, vote } = require("./controller");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -12,9 +12,9 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("Hello Raters!");
 });
-app.post("/addImages", addImages);
-app.get("/random", selectRandom);
-app.post("/vote", vote);
+// app.post("/addImages", addImages);
+// app.get("/random", selectRandom);
+// app.post("/vote", vote);
 
 app.listen(port, () => {
   console.log(`Rating-backend listening at http://localhost:${port}`);
